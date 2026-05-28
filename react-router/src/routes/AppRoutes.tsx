c54407580d3935323a6c68adc4a52cf8b8fa1f15
KauaGabriell <kauagabriell64@gmail.com>
2026-05-28 10:45:03 -0300

    feat(routes): add products route

--- react-router/src/routes/AppRoutes.tsx
+++ react-router/src/routes/AppRoutes.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router";
 
 import { Home } from "../pages/Home";
+import { Products } from "../pages/Products";
 
 export function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />}></Route>
+      <Route path="/products" element={<Products />}></Route>
     </Routes>
   );
 }
