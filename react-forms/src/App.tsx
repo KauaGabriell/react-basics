d5543efea44cf8eb288230d1cb32f3ca26f23ac9
KauaGabriell <kauagabriell64@gmail.com>
2026-05-28 14:53:21 -0300

    feat(forms): add react rook form and manipulate form

--- react-forms/src/App.tsx
+++ react-forms/src/App.tsx
@@ -1,42 +1,79 @@
 import "./App.css";
-import { useState } from "react";
+import { Controller, useForm } from "react-hook-form";
+
+type FormData = {
+  name: string;
+  date: string;
+  subject: string;
+  description: string;
+};
 
 export default function App() {
-  const [name, setName] = useState("");
+  const { control, handleSubmit } = useForm<FormData>({
+    defaultValues: {
+      name: "",
+      date: "",
+      subject: "",
+      description: "",
+    },
+  });
 
-  function onSubmit(e: React.FormEvent<HTMLElement>) {
-    e.preventDefault();
-
-    setName("");
+  function onSubmit(data: FormData) {
+    console.log(data);
   }
 
   return (
     <div>
       <h1>Evento</h1>
 
-      <form onSubmit={onSubmit}>
-        <input
-          type="text"
-          placeholder="Nome do evento"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Controller
+          control={control}
+          name="name"
+          render={({ field }) => (
+            <input type="text" placeholder="Nome do evento" {...field} />
+          )}
         />
+
         <span className="error">Nome é obrigatório</span>
 
-        <input type="date" placeholder="Nome do evento" lang="pt-BR" />
+        <Controller
+          control={control}
+          name="date"
+          render={({ field }) => (
+            <input
+              type="date"
+              placeholder="Nome do evento"
+              lang="pt-BR"
+              {...field}
+            />
+          )}
+        />
 
-        <select defaultValue="">
-          <option value="" disabled>
-            Selecione...
-          </option>
+        <Controller
+          control={control}
+          name="subject"
+          render={({ field }) => (
+            <select {...field}>
+              <option value="" disabled>
+                Selecione...
+              </option>
 
-          <option value="technology">React</option>
-          <option value="entertainment">Node.js</option>
-          <option value="business">Javascript</option>
-          <option value="business">Typescript</option>
-        </select>
+              <option value="React">React</option>
+              <option value="Node">Node.js</option>
+              <option value="Javascript">Javascript</option>
+              <option value="Typescript">Typescript</option>
+            </select>
+          )}
+        />
 
-        <textarea placeholder="Descrição" rows={4} />
+        <Controller
+          control={control}
+          name="description"
+          render={({ field }) => (
+            <textarea placeholder="Descrição" rows={4} {...field} />
+          )}
+        />
 
         <button type="submit">Salvar</button>
       </form>
