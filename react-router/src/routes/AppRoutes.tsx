2bc7239c25564250c9412f6f04944fe10223665e
KauaGabriell <kauagabriell64@gmail.com>
2026-05-28 10:50:45 -0300

    feat(routes): add not found page

--- react-router/src/routes/AppRoutes.tsx
+++ react-router/src/routes/AppRoutes.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router";
 
 import { Home } from "../pages/Home";
+import { NotFound } from "../pages/NotFound";
 import { Products } from "../pages/Products";
 
 export function AppRoutes() {
@@ -8,6 +9,8 @@ export function AppRoutes() {
     <Routes>
       <Route path="/" element={<Home />}></Route>
       <Route path="/products" element={<Products />}></Route>
+      
+      <Route path="*" element={<NotFound />}></Route>
     </Routes>
   );
 }
