b0d7f4e21c8c1580f3f7b794e84becf832688e4e
KauaGabriell <kauagabriell64@gmail.com>
2026-05-28 11:12:32 -0300

    feat(home): add imperative navigation button

--- react-router/src/pages/Home.tsx
+++ react-router/src/pages/Home.tsx
@@ -1,10 +1,17 @@
+import { useNavigate } from "react-router";
 export function Home() {
+  const navigate = useNavigate();
+
   return (
     <div>
       <h1>Página Home</h1>
       <nav>
         <a href="/products">Produtos</a>
         <a href="/products?category=tvs">Categoria</a>
+
+        <button type="button" onClick={() => navigate("/products")}>
+          Ver Produtos
+        </button>
       </nav>
     </div>
   );
